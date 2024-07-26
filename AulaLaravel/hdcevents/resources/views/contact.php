b64009ad77937da5d11715a9d6c07228b274ed39
Bruno Leão <bruno.melosh@gmail.com>
2024-07-26 14:42:32 -0400

    atualizacoes da aula do curso de laravel

---
+++ AulaLaravel/hdcevents/resources/views/contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact</title>
+</head>
+<body>
+  <h1>Página de contatos</h1>
+    @if(10 > 25)
+        <p>Voltar para a home</p>
+    @endif
+</body>
+</html>
